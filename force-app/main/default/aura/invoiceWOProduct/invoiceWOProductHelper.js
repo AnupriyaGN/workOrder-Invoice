3d9e280a70dabb3f27d12be1afdf358dcc53a7a2
saumya-growth <somias@growthnatives.com>
2023-06-30 14:02:28 +0530

    Revert "29-06-2023 commit"
    This reverts commit cbe3480d10d937229c9e29591fc6401736e684a6.

--- force-app/main/default/aura/invoiceWOProduct/invoiceWOProductHelper.js
+++ force-app/main/default/aura/invoiceWOProduct/invoiceWOProductHelper.js
@@ -27,7 +27,7 @@
         toastEvent.fire();
     },
     getFieldSetList : function(component,event,objectName){
-        var action = component.get('c.getAllfeildsets');
+         var action = component.get('c.getAllfeildsets');
         action.setParams({
             ObjectName : objectName,
         });
@@ -42,42 +42,6 @@
             } 
         });
         $A.enqueueAction(action);
-    },
-    SetOpportunityFields : function(component,event,helper){
-		 var action = component.get("c.objectFeildsSecond");  
-        action.setParams({ 
-            objectName : 'Opportunity',
-            fieldType : 'All',
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if(state === "SUCCESS"){
-                var optionsList = [];
-                var allValues = response.getReturnValue();
-                console.log("all Oppo Fields Values =>",allValues);
-                component.set('v.picklistOptionsList', allValues); 
-                component.set('v.picklistOptionsListDuplicate', allValues);
-                
-                console.log("AllOppoField",JSON.stringify(component.get('v.picklistOptionsListDuplicate')));
-               
-              
-                
-              
-            }
-            else if(state === "ERROR") {
-                var errors = response.getError();
-                if(errors){
-                    if(errors[0] && errors[0].message){
-                        console.log("Error Message1: " + errors[0].message);
-                    }
-                }
-                else{
-                    console.log("Unknown Error");
-                }
-            }
-        });        
-        $A.enqueueAction(action);  
-    
     }
     
 })
